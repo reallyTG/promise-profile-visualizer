a86edf92790f6b9348f995f154839ac8a96445ca
Alexi Turcotte <alexi.s.f.turcotte@gmail.com>
2020-05-29 11:56:33 -0400

    cleaning

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryLabel, VictoryChart, VictoryTheme, VictoryTooltip, VictoryVoronoiTooltip, VictoryZoomContainer } from 'victory';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryTooltip, VictoryZoomContainer } from 'victory';
 import jsonData from '../../data/the.json';
 import Prism from "prismjs"
 import { Multiselect } from "multiselect-react-dropdown"
 
 // For the code region:
-import Editor from 'react-simple-code-editor'
-import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/themes/prism.css';
